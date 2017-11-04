fd8ba04db2220171e011d6b0d2d17317369767fc
David <davids@codeprecise.com>
2017-11-04 12:31:25 +0200

    Using fgets with 256 limit

--- FindString.c
+++ FindString.c
@@ -6,14 +6,10 @@
 
 #define OK                            0
 #define FILE_OPEN_FAILED            (-1)
+#define FILE_READ_FAILED			(-2)
 
-#define WRONG_NUMBER_OF_ARGUMENTS   "Wrong number of arguments." \
-                                    " You need to enter at least two" \
-                                    " arguments 1. a word to search for." \
-                                    " 2.an existing file name. \n" \
-
-#define FILE_OPEN_ERROR            ": No such file or directory.\n" \
-
+#define MAX_CHARS_IN_LINE			(256)
+#define MAX_BYTES_IN_LINE			(256 + 2 + 1) // allow \n or \r\n at the end of the line + null
 
 /**
  * Prints a line
@@ -47,13 +43,18 @@ bool isCaseInsensitiveEqual(char left, char right)
  */
 void findWordsInLine(char *printFileName, char *wordToFind, char *line)
 {
-
-	size_t wordLength = strlen(wordToFind);
+	/* As the source of line is the fgets function it is guaranteed that
+	 * it contains a null somewhere in its allocated space.
+	 */
 	size_t lineLength = strlen(line);
+	size_t wordLength = strlen(wordToFind);
 
-	/* letterCount counts the number of letters from the line have been read
+	/* letterCount counts the number of consecutive letters from the line have been read
 	 * that are the same as those in wordToFind.
-	 * If its value is -1, the current position cannot be the start of wordToFind.
+	 * The maximum number of chars in wordToFind is 512 so letterCount will never
+	 * become negative when counting the number of letters.
+	 * Therefore we can give -1 a special meaning.
+	 * If letterCount is -1, the current position cannot be the start of wordToFind.
 	 */
 
 	int letterCount = 0;
@@ -62,40 +63,27 @@ void findWordsInLine(char *printFileName, char *wordToFind, char *line)
 		char c = line[i];
 		if (c == ' ' || c == '\r' || c == '\n') {
 
-			/* We recognize the word only if the next character is a space, a new line
-			 * or the end of the line. The end of the line is detected after the loop */
-
 			if (letterCount == (int) wordLength) {
 				printLine(printFileName, line);
 				return;
 			}
-
-			/* After having seen a space or newline, we can restart the search for wordToFind */
 			letterCount = 0;
 		}
 		else if (letterCount != -1) {
-			if (letterCount == (int) wordLength) {
-				/* If we have already seen all letters of wordToFind
-				 * then that word does not count because this character is not a space
-				 * Moreover, we cannot start a new search yet because the previous letter
-				 * was not a space, so reset letterCount to -1 */
-				letterCount = -1;
-			}
-			else if (isCaseInsensitiveEqual(c, wordToFind[letterCount])) {
-				/* We are on track to find the word, look for the next letter */
+			if ((letterCount < (int) wordLength) &&
+					isCaseInsensitiveEqual(c, wordToFind[letterCount])) {
 				letterCount++;
 			}
 			else {
-				/* The match has failed.*/
 				letterCount = -1;
 			}
 		}
 	}
 
 	/* This handles the case that the wordToFind was at the end of the line and the line
-	 * did not end in a newline (the last line in the file)  */
+	 * did not end in a newline (i.e. the last line in the file)  */
 
-	if (letterCount == (int) wordLength) {
+	if (letterCount == (int)wordLength) {
 		printLine(printFileName, line);
 	}
 }
@@ -107,38 +95,35 @@ void findWordsInLine(char *printFileName, char *wordToFind, char *line)
  * @param printFileName a pointer to the print files name
  * @return o if there were no errors, and 1 if there were
  */
-int findWordsInFile(char *wordToFind, char *fileName, char *printFileName)
+int findWordsInFile(char *wordToFind, char *fileName, char* printFilename)
 {
-
 	FILE *fp = fopen(fileName, "r");
 	if (fp == 0) {
 		return FILE_OPEN_FAILED;
 	}
 
-	char *line = 0;
-	size_t size = 0;
+	char line [MAX_BYTES_IN_LINE];
 
 	while (true) {
-		/* see explanation for getline at http://man7.org/linux/man-pages/man3/getline.3.html
-		 * In the first call, line will be allocated by getline and the size of the allocation
-		 * is placed in size.
-		 * On every following call, we pass in the same allocated pointer. If it is large enough
-		 * it will be reused. If not it will be freed and replaced by one that is large enough.
-		*/
-		ssize_t lineLength = getline(&line, &size, fp);
-		if (lineLength == -1) {
-			/* The end of the file has been reached */
-			break;
+
+		char* str = fgets(line, MAX_BYTES_IN_LINE, fp);
+		if (str == 0) {
+			if (feof(fp)) {
+				/* The end of the file has been reached */
+				break;
+			}
+			else{
+				return FILE_READ_FAILED;
+			}
 		}
-		findWordsInLine(printFileName, wordToFind, line);
+		findWordsInLine(printFilename, wordToFind, line);
 	}
 
 	/* line does not need to be freed in the loop, because as described above, it
 	 * is reused if large enough and freed internally and replaced if not. */
 
-	if (line != 0) {
-		free(line);
-	}
+
+
 	fclose(fp);
 
 	return OK;
@@ -153,12 +138,16 @@ int findWordsInFile(char *wordToFind, char *fileName, char *printFileName)
 int main(int argc, char *argv[])
 {
 	if (argc < 3) {
-		fprintf(stderr, WRONG_NUMBER_OF_ARGUMENTS);
+		fprintf(stderr,
+            "Wrong number of arguments.\n"
+            "You need to enter at least two arguments\n"
+            "1. a word to search for.\n"
+            "2. an existing file name.\n");
 		return EXIT_FAILURE;
 	}
 
 	char *wordToFind = argv[1];
-	int fileCount = argc - 2;
+	bool shouldPrintFilename = argc > 3;
 
 	for (int i = 2; i < argc; i++) {
 
@@ -166,12 +155,13 @@ int main(int argc, char *argv[])
 		int error = findWordsInFile(
 				wordToFind,
 				fileName,
-				fileCount == 1 ? 0 : fileName);
+				shouldPrintFilename ? fileName : 0);
+
 		if (error != OK) {
-			//TODO: print the file name before
-			fprintf(stderr, FILE_OPEN_ERROR);
+			fprintf(stderr, "grep: %s: No such file or directory\n", fileName);
 			return EXIT_FAILURE;
 		}
 	}
+
 	return EXIT_SUCCESS;
 }
