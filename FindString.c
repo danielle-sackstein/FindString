608b47e868da946870a8dbaff1ee53b1dae2f391
David <davids@codeprecise.com>
2017-11-04 13:49:55 +0200

    Merge branch 'master' of https://github.com/danielle-sackstein/FindString
    # Conflicts:
    #	FindString.c

--- FindString.c
+++ FindString.c
@@ -6,22 +6,59 @@
 
 #define OK                            0
 #define FILE_OPEN_FAILED            (-1)
-#define FILE_READ_FAILED			(-2)
+#define FILE_READ_FAILED            (-2)
+#define FILE_CLOSE_FAILED           (-3)
 
-#define MAX_CHARS_IN_LINE			(256)
-#define MAX_BYTES_IN_LINE			(256 + 2 + 1) // allow \n or \r\n at the end of the line + null
+#define MAX_CHARS_IN_LINE            (256)
+#define MAX_BYTES_IN_LINE            (MAX_CHARS_IN_LINE + 2 + 1) // allow \n or \r\n at the end of the line + null
+
+
+/**
+ * print usage
+ */
+void printUsage() {
+    fprintf(stderr, "Wrong number of arguments.\n"
+            "You need to enter at least two arguments\n"
+            "1. a word to search for.\n"
+            "2. an existing file name.\n");
+}
+
+/**
+ * prints the error.
+ * @param fileName the name of the file that had an error
+ * @param error the error that was found
+ */
+void printError(const char *fileName, int error) {
+    switch (error) {
+        case FILE_OPEN_FAILED: {
+            fprintf(stderr, "grep: %s: No such file or directory\n", fileName);
+            break;
+        }
+        case FILE_READ_FAILED: {
+            fprintf(stderr, "grep: %s: failed to read the file\n", fileName);
+            break;
+        }
+        case FILE_CLOSE_FAILED: {
+            fprintf(stderr, "grep: %s: failed to close the file\n", fileName);
+            break;
+        }
+        default: {
+            fprintf(stderr, "unexpected error\n");
+
+        }
+    }
+}
 
 /**
  * Prints a line
  * @param printFileName a pointer to the files name
  * @param line a pointer to the line to print
  */
-void printLine(char *printFileName, char *line)
-{
-	if (printFileName != 0) {
-		printf("%s:", printFileName);
-	}
-	printf("%s", line);
+void printLine(char *printFileName, char *line) {
+    if (printFileName != 0) {
+        printf("%s:", printFileName);
+    }
+    printf("%s", line);
 }
 
 /**
@@ -30,9 +67,8 @@ void printLine(char *printFileName, char *line)
  * @param right second character
  * @return true if they are equal, false otherwise.
  */
-bool isCaseInsensitiveEqual(char left, char right)
-{
-	return toupper(left) == toupper(right) ? true : false;
+bool isCaseInsensitiveEqual(char left, char right) {
+    return toupper(left) == toupper(right) ? true : false;
 }
 
 /**
@@ -41,51 +77,46 @@ bool isCaseInsensitiveEqual(char left, char right)
  * @param wordToFind a pointer to the word
  * @param line a pointer to the line that is checked.
  */
-void findWordsInLine(char *printFileName, char *wordToFind, char *line)
-{
-	/* As the source of line is the fgets function it is guaranteed that
-	 * it contains a null somewhere in its allocated space.
-	 */
-	size_t lineLength = strlen(line);
-	size_t wordLength = strlen(wordToFind);
+bool findWordsInLine(char *wordToFind, char *line) {
 
-	/* letterCount counts the number of consecutive letters from the line have been read
-	 * that are the same as those in wordToFind.
-	 * The maximum number of chars in wordToFind is 512 so letterCount will never
-	 * become negative when counting the number of letters.
-	 * Therefore we can give -1 a special meaning.
-	 * If letterCount is -1, the current position cannot be the start of wordToFind.
-	 */
+    /* As the source of line is the fgets function it is guaranteed that
+     * it contains a null somewhere in its allocated space.
+     */
 
-	int letterCount = 0;
+    size_t lineLength = strlen(line);
+    size_t wordLength = strlen(wordToFind);
 
-	for (size_t i = 0; i < lineLength; i++) {
-		char c = line[i];
-		if (c == ' ' || c == '\r' || c == '\n') {
+    /* letterCount counts the number of consecutive letters from the line have been read
+     * that are the same as those in wordToFind.
+     * The maximum number of chars in wordToFind is 512 so letterCount will never
+     * become negative when counting the number of letters.
+     * Therefore we can give -1 a special meaning.
+     * If letterCount is -1, the current position cannot be the start of wordToFind.
+     */
 
-			if (letterCount == (int) wordLength) {
-				printLine(printFileName, line);
-				return;
-			}
-			letterCount = 0;
-		}
-		else if (letterCount != -1) {
-			if ((letterCount < (int) wordLength) &&
-					isCaseInsensitiveEqual(c, wordToFind[letterCount])) {
-				letterCount++;
-			}
-			else {
-				letterCount = -1;
-			}
-		}
-	}
+    int letterCount = 0;
 
-	/* This handles the case that the wordToFind was at the end of the line and the line
-	 * did not end in a newline (i.e. the last line in the file)  */
+    for (size_t i = 0; i < lineLength; i++) {
+        char c = line[i];
+        if (c == ' ' || c == '\r' || c == '\n') {
 
-	if (letterCount == (int)wordLength) {
-		printLine(printFileName, line);
-	}
+            if (letterCount == (int) wordLength) {
+                return true;
+            }
+            letterCount = 0;
+        } else if (letterCount != -1) {
+            if ((letterCount < (int) wordLength) &&
+                isCaseInsensitiveEqual(c, wordToFind[letterCount])) {
+                letterCount++;
+            } else {
+                letterCount = -1;
+            }
+        }
+    }
+
+    /* This handles the case that the wordToFind was at the end of the line and the line
+     * did not end in a newline (i.e. the last line in the file)  */
+    return (letterCount == (int) wordLength);
 }
 
 /**
@@ -95,38 +126,35 @@ void findWordsInLine(char *printFileName, char *wordToFind, char *line)
  * @param printFileName a pointer to the print files name
  * @return o if there were no errors, and 1 if there were
  */
-int findWordsInFile(char *wordToFind, char *fileName, char* printFilename)
-{
-	FILE *fp = fopen(fileName, "r");
-	if (fp == 0) {
-		return FILE_OPEN_FAILED;
-	}
+int findWordsInFile(char *wordToFind, char *fileName, char *printFileName) {
+    FILE *fp = fopen(fileName, "r");
+    if (fp == 0) {
+        return FILE_OPEN_FAILED;
+    }
 
-	char line [MAX_BYTES_IN_LINE];
+    char line[MAX_BYTES_IN_LINE];
 
-	while (true) {
+    /*will never be an infinitive loop because we always check if we got to the EOF*/
+    while (true) {
 
-		char* str = fgets(line, MAX_BYTES_IN_LINE, fp);
-		if (str == 0) {
-			if (feof(fp)) {
-				/* The end of the file has been reached */
-				break;
-			}
-			else{
-				return FILE_READ_FAILED;
-			}
-		}
-		findWordsInLine(printFilename, wordToFind, line);
-	}
+        char *str = fgets(line, MAX_BYTES_IN_LINE, fp);
+        if (str == 0) {
+            if (feof(fp)) {
+                /* The end of the file has been reached */
+                break;
+            } else {
+                return FILE_READ_FAILED;
+            }
+        }
+        bool found = findWordsInLine(wordToFind, line);
+        if (found) {
+            printLine(printFileName, line);
+        }
+    }
 
-	/* line does not need to be freed in the loop, because as described above, it
-	 * is reused if large enough and freed internally and replaced if not. */
+    bool success = (0 == fclose(fp));
 
-
-
-	fclose(fp);
-
-	return OK;
+    return success ? OK : FILE_CLOSE_FAILED;
 }
 
 /**
@@ -135,33 +163,30 @@ int findWordsInFile(char *wordToFind, char *fileName, char* printFilename)
  * @param argv a pointer to the arguments
  * @return o if there were no errors, and 1 if there were
  */
-int main(int argc, char *argv[])
-{
-	if (argc < 3) {
-		fprintf(stderr,
-            "Wrong number of arguments.\n"
-            "You need to enter at least two arguments\n"
-            "1. a word to search for.\n"
-            "2. an existing file name.\n");
-		return EXIT_FAILURE;
-	}
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printUsage();
+        return EXIT_FAILURE;
+    }
 
-	char *wordToFind = argv[1];
-	bool shouldPrintFilename = argc > 3;
+    char *wordToFind = argv[1];
 
-	for (int i = 2; i < argc; i++) {
+    bool shouldPrintFilename = argc > 3;
 
-		char *fileName = argv[i];
-		int error = findWordsInFile(
-				wordToFind,
-				fileName,
-				shouldPrintFilename ? fileName : 0);
+    for (int i = 2; i < argc; i++) {
 
-		if (error != OK) {
-			fprintf(stderr, "grep: %s: No such file or directory\n", fileName);
-			return EXIT_FAILURE;
-		}
-	}
+        char *fileName = argv[i];
+        int error = findWordsInFile(
+                wordToFind,
+                fileName,
+                shouldPrintFilename ? fileName : 0);
 
-	return EXIT_SUCCESS;
+        if (error != OK) {
+            printError(fileName, error);
+            return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
+
